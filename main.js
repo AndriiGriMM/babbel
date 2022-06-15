a3b4bc141cbcbafac05d91871daf38996820ba3e
AndriiGriMM <webdevgrimm@gmail.com>
2022-06-15 15:32:51 +0300

    15.06.22 3

--- main.js
+++ main.js
@@ -2,35 +2,28 @@ function Slider(params) {
 
   params.next.addEventListener('click', function (event) {
     params.index = params.index + 1
-
     updateSelection();
+    params.prev.disabled = false
+    this.disabled = params.index === params.maxLenghs
+    
   });
 
   params.prev.addEventListener('click', function () {
     params.index = params.index - 1
+    params.next.disabled = false;
+    this.disabled = params.index === 0
     updateSelection();
   });
 
-  function checkBtnNext() {
-    params.next.disabled = params.index === params.maxLenghs
-  }
-
-  function checkBtnPrev() {
-    params.prev.disabled = params.index === 0 || params.index !== 0
-  }
-
   function updateSelection() {
-
     const active = params.list.querySelector('.active');
-
     if (active) active.classList.remove('active');
-
     params.items[params.index].classList.add('active');
 
   }
-  checkBtnPrev()
-  checkBtnNext()
+
 }
+
 const slider = new Slider({
   prev: document.querySelector('.select-prev'),
   next: document.querySelector('.select-next'),
