a4de62d79de929cc65c52a9a8683b4f6a40e5a1f
AndriiGriMM <webdevgrimm@gmail.com>
2022-06-15 14:40:05 +0300

    test

--- main.js
+++ main.js
@@ -1,45 +1,50 @@
-const btn = {
+function Slider(params) {
 
+  params.next.addEventListener('click', function (event) {
+    params.index = params.index + 1
+
+    console.log(params.maxLenghs)
+    updateSelection();
+    checkBtnNext()
+    checkBtnPrev()
+  });
+
+  params.prev.addEventListener('click', function () {
+    params.index = params.index - 1
+
+    updateSelection();
+    checkBtnPrev()
+    checkBtnNext()
+  });
+
+  function checkBtnNext() {
+    params.next.disabled = params.index === params.maxLenghs
+    console.log(params.index)
+  }
+
+  function checkBtnPrev() {
+    params.prev.disabled = params.index === 0
+  }
+
+  function updateSelection() {
+
+    const active = params.list.querySelector('.active');
+
+    if (active) active.classList.remove('active');
+
+    params.items[params.index].classList.add('active');
+
+  }
+
+}
+const slider = new Slider({
   prev: document.querySelector('.select-prev'),
   next: document.querySelector('.select-next'),
-
-};
-
-const list = document.querySelector(".list-items");
-const items = list.querySelectorAll("li");
-let index = 0;
-const button = document.querySelector('.select-next');
-
-btn.next.addEventListener('click', function () {
-  index = index+1 
-  updateSelection();
-  checkBtn()
-
-});
-
-btn.prev.addEventListener('click', function () {
-  index = index - 1
-  updateSelection();
-  checkBtn()
-
-});
-
-function checkBtn() {
-  const maxLenghs = items.length - 1
-  index === 0 ? btn.prev.disabled = true 
-  : btn.prev.disabled = false
-  index === maxLenghs ? btn.next.disabled = true 
-  : btn.next.disabled = false
+  list: document.querySelector('.list-items'),
+  items: document.querySelectorAll("li"),
+  index: 0,
+  maxLenghs: document.querySelectorAll("li").length - 1
 
 }
-
-function updateSelection() {
-
-  const active = list.querySelector('.active');
-
-  if (active) active.classList.remove('active');
-
-  items[index].classList.add('active');
-
-}
+)
 
