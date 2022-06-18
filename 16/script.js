9ff3a1f8d80446cc35150aafca79f9fc2b2b1d71
AndriiGriMM <webdevgrimm@gmail.com>
2022-06-18 17:01:28 +0300

    add form

--- 16/script.js
+++ 16/script.js
@@ -12,55 +12,17 @@ function FormValidate(form) {
         for (let i = 0; i < _elements.length; i++) {
             const element = _elements[i];
             const reqMessage = element.dataset.req;
-            const emailMessage = element.dataset.email;
-            const minLengthMessage = element.dataset.min_message;
-            const passwordMessage = element.dataset.password;
-            this.checkElement (element)
-            if (passwordMessage) {
-                this.validPassword(passwordMessage)
-            }
+            this.cleanError (element)
             if (reqMessage) {
                 this.required(element, reqMessage);
             }
-            if (emailMessage) {
-                this.validEmail(element, emailMessage);
-            }
-            if (minLengthMessage) {
-                this.minLength(element, minLengthMessage);
-            }
         }
     }
-
-    this.validPassword = function (message) {
-        const allPasswordElement = form.querySelectorAll("input[type='password']");
-        Array.from(allPasswordElement).map(item => {
-            if (allPasswordElement[0].value !== allPasswordElement[1].value) {
-                this.errorTemplate(item, message);
-            }
-        })
-    }
-
     this.required = function (element, message) {
         const notValidString = element.value.length === 0;
         const notValidCheckBox = element.type === 'checkbox' && element.checked === false;
         if (notValidString || notValidCheckBox) {
             this.errorTemplate(element, message);
-        }
-    }
-    this.validEmail = function (element, message) {
-        const emailString = element.value;
-        const reg = emailString.match(/^((?!\.)[\w_.]*[^.])(@\w+)(\.\w+(\.\w+)?[^.\W])$/);
-        if (Boolean(reg) === false) {
-            this.errorTemplate(element, message);
-        }
-    }
-
-    this.minLength = function (element, message) {
-        const minLength = element.dataset.min_length;
-        const elementLength = element.value.length;
-        const changeMassage = message.replace('N', minLength);
-        if (elementLength < minLength) {
-            this.errorTemplate(element, changeMassage);
         }
     }
 
@@ -72,8 +34,13 @@ function FormValidate(form) {
         }
     }
 
-    this.checkElement = function (element) {
-        element.parentNode.classList.remove(_errorWrapperClass)
+
+    this.cleanError = function (element) {
+        const parent = element.closest(`.${_parentItemClass}`);
+        if (parent !== null && parent.classList.contains(_errorWrapperClass) === true) {
+           element.parentNode.classList.remove(_errorWrapperClass);
+           element.parentNode.querySelector(`.${_errorItemClass}`).remove()
+        }
     }
 }
 
