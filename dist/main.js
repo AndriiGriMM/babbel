e8dcc318834bdbc6cef2a3b840cc41ee00ccb4d4
AndriiGriMM <webdevgrimm@gmail.com>
2022-07-11 18:09:21 +0300

    babel-3 commit

--- dist/main.js
+++ dist/main.js
@@ -1,11 +1,5 @@
 "use strict";
 
-function _construct(Parent, args, Class) { if (_isNativeReflectConstruct()) { _construct = Reflect.construct.bind(); } else { _construct = function _construct(Parent, args, Class) { var a = [null]; a.push.apply(a, args); var Constructor = Function.bind.apply(Parent, a); var instance = new Constructor(); if (Class) _setPrototypeOf(instance, Class.prototype); return instance; }; } return _construct.apply(null, arguments); }
-
-function _isNativeReflectConstruct() { if (typeof Reflect === "undefined" || !Reflect.construct) return false; if (Reflect.construct.sham) return false; if (typeof Proxy === "function") return true; try { Boolean.prototype.valueOf.call(Reflect.construct(Boolean, [], function () {})); return true; } catch (e) { return false; } }
-
-function _setPrototypeOf(o, p) { _setPrototypeOf = Object.setPrototypeOf ? Object.setPrototypeOf.bind() : function _setPrototypeOf(o, p) { o.__proto__ = p; return o; }; return _setPrototypeOf(o, p); }
-
 function ownKeys(object, enumerableOnly) { var keys = Object.keys(object); if (Object.getOwnPropertySymbols) { var symbols = Object.getOwnPropertySymbols(object); enumerableOnly && (symbols = symbols.filter(function (sym) { return Object.getOwnPropertyDescriptor(object, sym).enumerable; })), keys.push.apply(keys, symbols); } return keys; }
 
 function _objectSpread(target) { for (var i = 1; i < arguments.length; i++) { var source = null != arguments[i] ? arguments[i] : {}; i % 2 ? ownKeys(Object(source), !0).forEach(function (key) { _defineProperty(target, key, source[key]); }) : Object.getOwnPropertyDescriptors ? Object.defineProperties(target, Object.getOwnPropertyDescriptors(source)) : ownKeys(Object(source)).forEach(function (key) { Object.defineProperty(target, key, Object.getOwnPropertyDescriptor(source, key)); }); } return target; }
@@ -14,7 +8,7 @@ function _defineProperty(obj, key, value) { if (key in obj) { Object.definePrope
 
 var data = [undefined, 2, 3, 4, 5, 'sd'];
 
-function EcmaScript() {
+var ecmaScript = function ecmaScript() {
   var arg1 = arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : "error";
   var arg2 = arguments.length > 1 ? arguments[1] : undefined;
   var arg3 = arguments.length > 2 ? arguments[2] : undefined;
@@ -23,8 +17,8 @@ function EcmaScript() {
     arr[_key - 3] = arguments[_key];
   }
 
-  arguments.length < 5 ? console.log("ERROR") : console.log("".concat(arg1, "-").concat(arg2, "-").concat(arg3));
+  arr.length < 2 ? console.log("ERROR") : console.log("".concat(arg1, "-").concat(arg2, "-").concat(arg3));
   console.log(_objectSpread({}, arr));
-}
+};
 
-_construct(EcmaScript, data);
+ecmaScript.apply(void 0, data);
--- src/main.js
+++ src/main.js
@@ -1,6 +1,6 @@
 const data = [undefined, 2, 3, 4, 5, 'sd'];
-function EcmaScript(arg1 = "error", arg2, arg3, ...arr) {
-  arguments.length < 5 ? console.log("ERROR") : console.log(`${arg1}-${arg2}-${arg3}`)
-  console.log({ ...arr })
+const ecmaScript = (arg1 = "error", arg2, arg3, ...arr) => {
+  arr.length < 2 ? console.log("ERROR") : console.log(`${arg1}-${arg2}-${arg3}`);
+  console.log({ ...arr });
 }
-new EcmaScript(...data); 
+ecmaScript(...data); 
